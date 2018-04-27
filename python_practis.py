bf0a0d430de525d55e43e012b468393bf3da5008
ajajul <33230669+ajajul@users.noreply.github.com>
2018-04-27 11:34:51 +0530

    delete some thing

--- python_practis.py
+++ python_practis.py
@@ -3,6 +3,5 @@ print('my name is ajajul')
 print('I just add new data')
 print('Now ging to do new commit')
 print('create new branch')
-print('because I am not sure about It')
-print('updating the master branch')
+
 
